a73822df54be888b66f7f7cbfd7bb0deb441e604
Ella Musina <emusina@Ellas-MacBook-Pro-2.local>
2018-07-11 13:12:11 -0700

    Setting up with images processing via webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { ImageminWebpackPlugin } = require('imagemin-webpack');
+const ImageMinGifsicle = require('imagemin-gifsicle');
+
 module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'js/main.js',
+  },
   module: {
     rules: [
       {
@@ -7,9 +17,32 @@ module.exports = {
         use: { loader: 'babel-loader' },
       },
       {
-
-      }
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/',
+            },
+          },
+        ],
+      },
     ]
   },
-  plugins: []
+  plugins: [
+    new ImageminWebpackPlugin({
+      imageminOptions: {
+        bail: false,
+        plugins: ImageMinGifsicle({
+          interlaced: true,
+          optimizationLevel: 5,
+        }),
+      },
+    }),
+    new CopyWebpackPlugin([
+      { from: './img/**/*.{jpg,jpeg,png,gif}', to: '' },
+    ]),
+  ]
 };
